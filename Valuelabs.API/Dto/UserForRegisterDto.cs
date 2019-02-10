2be48bb4da393783212285c3dbaa644dbf9dff52
CSZCreations <chanakyasrinu01@gmail.com>
2019-02-10 21:10:24 +0530

    login in middle

--- Valuelabs.API/Dto/UserForRegisterDto.cs
+++ Valuelabs.API/Dto/UserForRegisterDto.cs
@@ -1,9 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Valuelabs.API.Dto
 {
     public class UserForRegisterDto
     {
+        [Required]
         public string Username{get; set;}
-
+        [Required]
+        [StringLength(8,MinimumLength =4,ErrorMessage="Password must be between 4 to 8")]
         public string Password {get;set;}
         
     }
